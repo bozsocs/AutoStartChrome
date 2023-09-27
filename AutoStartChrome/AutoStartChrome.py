e30a7cd460f1181d8fbb4b152d5f8f8c56fb6291
bozsocsa <124932021+bozsocs@users.noreply.github.com>
2023-09-28 01:43:51 +0200

    Add files via upload

--- AutoStartChrome/AutoStartChrome.py
+++ AutoStartChrome/AutoStartChrome.py
@@ -1,26 +1,23 @@
 import os
 import subprocess
+import time
 
-def open_chrome_with_website():
-    chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-    website_url = "https://studio.youtube.com/video/2ohbhwJ2hCA/livestreaming"
-    
-    # Check if Chrome is already running
-    if not is_chrome_running():
-        # Launch Chrome with the specified website
-        subprocess.Popen([chrome_path, website_url])
-    else:
-        print("Google Chrome is already running.")
+# A Google Chrome elérési útja (a saját telepítési útvonaladra módosítsd)
+chrome_path = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
-def is_chrome_running():
-    # Get the list of running processes
-    process_list = os.popen('tasklist').read().lower()
-    
-    # Check if "chrome.exe" is present in the process list
-    if "chrome.exe" in process_list:
-        return True
-    else:
-        return False
+# A megnyitni kívánt weboldal URL-je
+url = "https://studio.youtube.com/video/2ohbhwJ2hCA/livestreaming"
 
-# Run the function to open Google Chrome with the specified website
-open_chrome_with_website()
+# Ellenőrizzük, hogy a Google Chrome telepítve van-e
+if os.path.exists(chrome_path):
+    try:
+        # Indítsuk el a Google Chrome böngészőt a megadott URL-lel
+        subprocess.Popen([chrome_path, url])
+        print("Google Chrome elindítva és megnyitva a weboldalt.")
+    except Exception as e:
+        print(f"Hiba történt: {e}")
+else:
+    print("Google Chrome nem található a megadott elérési úton.")
+
+# Várunk egy rövid ideig (pl. 5 másodperc) a weboldal betöltésére
+time.sleep(5)
